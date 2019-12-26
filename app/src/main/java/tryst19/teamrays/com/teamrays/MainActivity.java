7b38196a4c5d48a7b0bc9489b3e4934ccc7334ad
Vishal Kumar <vish.dev04@gmail.com>
2019-12-26 19:32:44 +0530

    Initial Commit

---
+++ app/src/main/java/tryst19/teamrays/com/teamrays/MainActivity.java
@@ -0,0 +1,13 @@
+package tryst19.teamrays.com.teamrays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
